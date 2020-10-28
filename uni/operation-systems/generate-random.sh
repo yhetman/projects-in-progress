9c05a019af81c67eac401a9c6a1148db84367223
yhetman <juliagetman5@gmail.com>
2020-10-28 17:42:19 +0200

    passed lab

--- uni/operation-systems/generate-random.sh
+++ uni/operation-systems/generate-random.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo dd if=/dev/urandom of=/mnt/DATA/projects-in-progress/uni/operation-systems/example bs=4 count=10000
+sudo dd if=/dev/urandom of=/mnt/DATA/projects-in-progress/uni/operation-systems/example bs=4 count=100000
