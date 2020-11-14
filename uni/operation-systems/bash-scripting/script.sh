b4ec3eeedd2f0eddb1d358148a3ae04e165f4031
yhetman <juliagetman5@gmail.com>
2020-11-14 19:15:13 +0200

    script done

--- uni/operation-systems/bash-scripting/script.sh
+++ uni/operation-systems/bash-scripting/script.sh
@@ -8,8 +8,8 @@ script_path=/mnt/DATA/projects-in-progress/uni/operation-systems/processes_threa
 
 R=12
 
-N1=1
-N2=1
+N1=2
+N2=2
 
 r1=10
 r2=12
@@ -68,7 +68,7 @@ function get_dir ()
 function start_script()
 {
 	get_dir $1
-	sudo dd if=/dev/urandom of=./file bs=4 count=10000
+	sudo dd if=/dev/urandom of=./file bs=4 count=10000 1>/dev/null 2>/dev/null
 	$2 $3 1>/dev/null
 	rm file
 }
@@ -76,7 +76,10 @@ function start_script()
 function create_archive()
 {
 	get_dir $1
-	tar -czf ../archive_from_$1_on_$2_level.tar.gz ./* 
+	# echo `pwd`
+	tar -czf archive_from_$1_on_$2_level.tar.gz *.txt
+	#rm *txt
+	mv archive_from_$1_on_$2_level.tar.gz ../
 }
 
 
@@ -114,55 +117,79 @@ function locate()
 
 function add_file_to_archive()
 {
-	location=`find $virtdir -iname *$1.txt`
-	archive_location=`find $virtdir -iname *$1_on_$2*.tar.gz`
-	mkdir /tmp/tempo/
-	tar -zxf $archive_location -C /tmp/tempo/ 2>/dev/null
-	mv $location /tmp/tempo/
+	loc=`find $virtdir -iname *output$1.txt`
+	archive_loc=`find $virtdir -iname *archive_from_$1_*.tar.gz`
 	get_dir $1
-	tar -czf ../archive_from_$1_on_$2_level.tar.gz -C /tmp/tempo/* 2>/dev/null
-	rm -rf /tmp/tempo
+	dest=`pwd`
+	# echo $loc
+	# echo $archive_loc
+	tar -xzf $archive_loc 2>/dev/null
+	rm $archive_loc
+	mv $loc .
+	tar -C ./ -czf $dest/archive_from_$1_on_$2_level.tar.gz *.txt 2>/dev/null
+	mv $dest/archive_from_$1_on_$2_level.tar.gz ../
 }
 
 
 function extract_archive()
 {
 	get_dir $2
-	destination=`pwd`
-	archive_location=`find $virtdir -iname *_on_$1*.tar.gz`
-	echo $destination
-	tar -zxf $archive_location -C $destination 2>/dev/null
-
+	destin=`pwd`
+	archive_location=`find $virtdir -iname archive_from_*_on_$1_level.tar.gz`
+	# echo $destin
+	tar -xzf $archive_location -C $destin 2>/dev/null
+	# ls -ClR
 }
 
 function finish()
 {
-	echo "Script has just finished its work! Thanks for using our service :)" \n
-
+	echo ""
+	echo ""
+	echo ""
+	echo -e "\e[1m\e[94m=========== Script has just finished its work! Thanks for coming :) ===========\e[21m"
+	echo " "
+	echo -e "\e[38;5;198m============== Here the recent locations of involved archives : ==============\e[93m"
+	find $virtdir -iname archive_from_*.tar.gz
+	echo ""
+	echo -e "\e[38;5;198m========================== Final content of archives =========================="
+	echo ""
+	echo -e "\e[38;5;198m======== First : \e[96m"
+	tar -tf $virtdir/1folder/2folder/3folder/4folder/5folder/6folder/7folder/8folder/9folder/10folder/11folder/archive_from_12_on_11_level.tar.gz
+	echo ""
+	echo -e "\e[38;5;198m======== Second : \e[96m"
+	tar -tf $virtdir/1folder/2folder/3folder/4folder/5folder/6folder/7folder/8folder/9folder/archive_from_10_on_9_level.tar.gz
+	echo -e "\e[39m"
 }
-check $dir
 
-if [[ -e $dir ]]; then
-	cd $dir
-fi
 
-PWD=`pwd`
+function main()
+{
+	check $dir
 
-recur_dirs $R
+	if [[ -e $dir ]]; then
+		cd $dir
+	fi
 
-start_script $r1 $script_path/processes_implementation.py $N1
-start_script $r2 $script_path/threads_implementation.py $N2
+	PWD=`pwd`
+	recur_dirs $R
 
-create_archive $r1 $a1
-create_archive $r2 $a2
+	start_script $r1 $script_path/processes_implementation.py $N1
+	start_script $r2 $script_path/threads_implementation.py $N2
 
-sort_and_locate $r1 $t1
-sort_and_locate $r2 $t2
+	create_archive $r1 $a1
+	create_archive $r2 $a2
 
-add_file_to_archive $r1 $a1
-add_file_to_archive $r2 $a2
+	sort_and_locate $r1 $t1
+	sort_and_locate $r2 $t2
 
-extract_archive $a1 $a3
-extract_archive $a2 $a4
+	add_file_to_archive $r1 $a1
+	add_file_to_archive $r2 $a2
 
-finish
+	extract_archive $a1 $a3
+	extract_archive $a2 $a4
+	finish
+}
+
+
+
+main
