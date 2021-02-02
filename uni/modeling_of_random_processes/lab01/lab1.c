e1d2f6feda22bc9006a653ed8ba7420fe44e5b82
yhetman <juliagetman5@gmail.com>
2021-02-02 02:45:38 +0200

    lab1

--- uni/modeling_of_random_processes/lab01/lab1.c
+++ uni/modeling_of_random_processes/lab01/lab1.c
@@ -6,44 +6,134 @@
 /*   By: yhetman <yhetman@student.unit.ua>                                    */
 /*                                                                            */
 /*   Created: 2021/01/30 19:38:46 by yhetman                                  */
-/*   Updated: 2021/01/30 20:09:41 by yhetman                                  */
+/*   Updated: 2021/02/02 01:54:00 by yhetman                                  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-double *multicong(unsigned int alpha, unsigned int M, unsigned int beta , double *randomNums, int amount)
+
+double *multicong(int amount)
 {
-	int j;
+	unsigned int	M,
+					alpha0,
+					beta;
+	int				j;
+	double			*alpha,
+					*rand_vars;
+
 	
 	j = 0;
-	randomNums[j] = alpha;
-	while (++j < amount)
-		randomNums[j] = (double)((int)(randomNums[j - 1] * beta) % M) / M;
-	return (randomNums);
+	alpha0 = 8;
+	beta = 77;
+	M = 10000000;
+	alpha = (double *)malloc((amount + 1)* sizeof(double));
+	rand_vars = (double *)malloc(amount * sizeof(double));
+	alpha[j] = alpha0;
+	while (++j < amount + 1)
+	{
+		alpha[j] = ((int)(alpha[j - 1] * beta) % M);
+		rand_vars[j - 1] = (double)(alpha[j - 1] / M);
+	}
+	free(alpha);
+	return (rand_vars);
+}
+
+
+int			find_minimum(double *a, int n)
+{
+	int		c,
+			index;
+	
+	index = 0;
+	for (c = 1; c < n; c++)
+		if (a[c] < a[index])
+			index = c;
+	return index;
+}
+
+
+int			find_maximum(double *a, int n)
+{
+	int		c,
+			index;
+	
+	index = 0;
+	for (c = 1; c < n; c++)
+		if (a[c] > a[index])
+			index = c;
+	return index;
+}
+
+typedef struct	s_dictionary
+{
+	double		key;
+	double			value;
+}				t_dictionary;
+
+void dictionaries(double minimal, int steps, double H, double *bvv, int n)
+{
+	t_dictionary	*dictionary1,
+					*dictionary2;
+	int				i, j;
+	double			phi;
+	
+	dictionary1 = (t_dictionary*)malloc(sizeof(t_dictionary) * steps);
+	dictionary2 = (t_dictionary*)malloc(sizeof(t_dictionary) * steps);
+	phi = 0;
+	for (i = 0; i < steps; i++)
+	{
+		dictionary1[i].key = minimal + H * i;
+		dictionary1[i].value = 0;
+		dictionary2[i].key = minimal + H * i;
+		dictionary2[i].value = 0;
+	}
+
+	for (i = 0; i < n; i++ )
+	{
+		for (j = 0; j < steps; j++)
+		{
+			if (bvv[i] > dictionary1[j].key && bvv[i] < (dictionary1[j].key + H))
+			{
+				dictionary1[j].value += 1;
+				dictionary2[j].value += bvv[i] - dictionary1[j].key + minimal;
+			}
+		}
+	}
+	for (i = 0; i < steps ; i++)
+	{
+		phi += pow((dictionary1[i].value - n / steps), 2) / (n / steps);
+//		printf("|%d|%.9f|\n",i, (double)(dictionary2[i].value / dictionary1[i].value));
+	}
+//	printf("Phi = %.9f\n", phi);
 }
 
 int main()
 {
-	unsigned int	M, alpha, beta;
 	int				amount,
-					i;
-	double			*randomNums;
-
+					i,
+					min,
+					max,
+					number_of_intervals;
+	double			H,
+					*rand_vars;
 
 	i = 0;
-	alpha = 80;
-	beta = 9;
-	M = 1000000;
-	amount = 100000;
-	randomNums = (double *)malloc(amount * sizeof(double));
-	while (i++ < amount)
-		randomNums = multicong(alpha, M, beta, randomNums, amount);
-	i = 0;
+	amount = 10000;
+	rand_vars = multicong(amount);
+	number_of_intervals = (int)(1 + log2(amount));
 	while(i++ < amount)
-		printf("|%d|%f|\n", i, randomNums[i]);
-	free(randomNums);
+		printf("%.9f\n", rand_vars[i]);
+//	printf("Number of intervals : %d\n",number_of_intervals);
+	min = find_minimum(rand_vars, amount);
+//	printf("Minimal value : %.9f\n", rand_vars[min]);
+	max = find_maximum(rand_vars, amount);
+//	printf("Maximal value : %.9f\n",rand_vars[max]);
+	H = (rand_vars[max] - rand_vars[min]) / number_of_intervals;
+	dictionaries(rand_vars[min], number_of_intervals, H, rand_vars, amount);
+	free(rand_vars);
 	return (0);
 }
 
---
+++ uni/modeling_of_random_processes/lab01/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc -o lab1 lab1.c -lm
+
+./lab1 > data_uni.txt 
