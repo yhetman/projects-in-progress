51e410776004aad0fa9f9eb3758a79e6675bef74
yhetman <juliagetman5@gmail.com>
2021-02-12 22:45:02 +0200

    lab02 {almost done}

--- uni/modeling_of_random_processes/lab02/lab02_2.c
+++ uni/modeling_of_random_processes/lab02/lab02_2.c
@@ -10,9 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> 
@@ -50,40 +48,39 @@ int main(int argc, char **argv)
 					j,
 					i;
 	double			*rand_vars;
-	time_t			seconds;
 	float			H_array[5];
-	int				s[4];
+	int				A_frequency,
+					frequency[4];
+	float			p[4] = {0.5, 0.25, 0.125, 0.125},
+					p_A_H[4] = {0.333, 0.25, 0.333, 0.5};
 
-	bzero(&s);
-	H_array = {0, 0.5, 0.75, 0.875, 1};
-	alpha = 0;
+
+	bzero((void*)frequency, sizeof(int) * 4);
+	bzero((void*)H_array, sizeof(float) * 5);
+	H_array[0] = 0;
+	for (i = 1; i < 5; i++)
+		H_array[i] = H_array[i - 1] + p[i - 1];
 	amount = atoi(argv[1]);
-	//p = atof(argv[2]);
-	//probability = 0;
-
-
-	// if (argc < 3)
-	// {
-	// 	printf("Usage:\n\n./lab02_2 {Number of elements to generate} {Probability to apply}\n");
-	// 	exit(1);
-	// }
-
 	rand_vars = multicong_generator(amount);
-
-    for (j = 0;j < amount; j++)
-    {
-    	i = 0;
-    	while (i < 5)
+	for (j = 0; j < amount; j++)
+    	for (i = 0; i < 5; i++)
     		if (rand_vars[j] > H_array[i] && rand_vars[j] < H_array[i + 1 ])
-    			s[i] += 1;
-    	time(&seconds);
-    	alpha = (int)seconds % amount;
-    	// printf("%d\n",alpha);
-    	if (rand_vars[alpha] < p)
-    		probability += 1;
-    }
-    probability /= 10000000;
-    printf("%s : %0.8f\n", "Relative frequency of Event  A", probability);
+    			frequency[i] += 1;
+    printf("H_1 = %d\n", frequency[0]);
+   	printf("H_2 = %d\n", frequency[1]);
+   	printf("H_3 = %d\n", frequency[2]);
+   	printf("H_4 = %d\n", frequency[3]);
+    printf("Probability_1 = %f\n", (float)frequency[0] / amount);
+   	printf("Probability_2 = %f\n", (float)frequency[1] / amount) ;
+   	printf("Probability_3 = %f\n", (float)frequency[2] / amount);
+   	printf("Probability_4 = %f\n", (float)frequency[3] / amount);
+   	A_frequency = 0;
+	for (j = 0; j < amount; j++)
+    	for (i = 0; i < 5; i++)
+    		if (rand_vars[j] > H_array[i] && rand_vars[j] < H_array[i + 1 ])
+    			if (rand_vars[j] < (H_array[i] + p_A_H[i] * p[i]))
+    				A_frequency += 1;
+	printf("Reletive frequency of event A = %d\t%f\n", A_frequency ,(float)A_frequency/amount);
 	free(rand_vars);
 	return (0);
 }
